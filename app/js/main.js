70374969a85f4df8080232e9e6003ab1ba091536
ArtZhevn <113771964+ArtZhevn@users.noreply.github.com>
2023-05-09 04:56:39 +0300

    cursor, animation

--- app/js/main.js
+++ app/js/main.js
@@ -9,11 +9,20 @@ likeButton.addEventListener('click', () => {
     likeButton.textContent = 'Мне нравится (0)';
     hiddenImages.forEach(image => {
       image.classList.add('hidden');
+      heartImage.classList.add('visible');
+      hiddenImages.forEach(image => {
+        image.classList.add('hidden');
+        image.classList.remove('visible');
+      });
     });
   } else {
     likeButton.textContent = 'Мне нравится (1)';
     hiddenImages.forEach(image => {
       image.classList.remove('hidden');
+      hiddenImages.forEach(image => {
+        image.classList.remove('hidden');
+        image.classList.add('visible');
+      });
     });
   }
   isLiked = !isLiked;
