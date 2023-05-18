b99e883407bc2b385dc4f66451dc248b080bef6d
ArtZhevn <113771964+ArtZhevn@users.noreply.github.com>
2023-05-18 06:36:14 +0300

    add vw and fix code

--- app/js/main.js
+++ app/js/main.js
@@ -6,17 +6,16 @@ let isLiked = false;
 
 likeButton.addEventListener('click', () => {
   if (isLiked) {
-    likeButton.textContent = 'Мне нравится (0)';
+    likeButton.textContent = 'мне нравится (0)';
     hiddenImages.forEach(image => {
       image.classList.add('hidden');
-      heartImage.classList.add('visible');
       hiddenImages.forEach(image => {
         image.classList.add('hidden');
         image.classList.remove('visible');
       });
     });
   } else {
-    likeButton.textContent = 'Мне нравится (1)';
+    likeButton.textContent = 'мне нравится (1)';
     hiddenImages.forEach(image => {
       image.classList.remove('hidden');
       hiddenImages.forEach(image => {
